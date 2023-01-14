b72cf43a6b86ea909158f7575721e74b2c029cdc
linajin03 <53534844+linajin03@users.noreply.github.com>
2023-01-14 16:25:53 -0500

    Suggestion translation functionality without the translation part!

--- translator_base.py
+++ translator_base.py
@@ -1,48 +1,31 @@
-from googletrans import Translator
 import cohere
-from cohere.classify import Example
-
-# examples = [
-#     Example("when r u coming 2day?", "informal"),
-#     Example("When are you arriving today?", "formal"),
-#     Example("are you coming today", "informal"),
-#     Example("What is ur ETA?", "informal"),
-#     Example("Could you please be a little more quiet?", "formal"),
-#     Example("Shut up", "informal"),
-#     Example("I don't care", "informal"),
-#     Example("I do not think this something I am focused on right now",
-#             "formal"),
-#     Example("whats your number", "informal"),
-#     Example("Could I have your contact information?", "formal"),
-#     Example("Do you have time to talk right now?", "formal"),
-#     Example("that's cap", "informal")
-# ]
+from googletrans import Translator
 
 co = cohere.Client('uAAXOvSLigsz9qCYyB0FhfSk7VGPL3pekqhynDfm')
 
 
 class SpeakEasyTranslator:
     """SpeakEasyTranslator: a class that translates the given input while also
-    generating suggestions
+    generating suggestions of other ways to say the phrase depending on the
+    desired level of formality.
+
     === Public Attributes ===
-    inputted_phrase: the inputted phrase by the user to translate
-    context: the context provided by the user to give more specific translation
+    inputted_phrase: the inputted phrase by the user to translate into English
     """
     inputted_phrase: str
-    context: str
 
-    def __init__(self, inputted_phrase: str, context: str) -> None:
-        """Initialize the instance variables.
+    def __init__(self, inputted_phrase: str) -> None:
+        """Initialize the instance variables, where .
         """
         self.inputted_phrase = inputted_phrase
-        self.context = context
 
-    def translate(self) -> str:
+    def translate_phrase(self) -> str:
         """Return the translated phrase of user input"""
         translator = Translator()
-        return translator.translate(self.inputted_phrase)
+        translated_text = translator.translate(self.inputted_phrase)
+        return translated_text.text
 
-    def suggest(self) -> str:
+    def suggest(self, tone: str) -> str:
         """Return different suggestions of the user's input depending on their
         desired context/tone.
         """
@@ -51,15 +34,15 @@ class SpeakEasyTranslator:
             prompt='This is paraphrasing program that generates different ways'
                    'of saying the inputted phrase in different formalities '
                    'according to who the user is talking to.\n--\n'
-                   'Phrase: Where are you?'
-                   '\nAudience: Friend'
-                   '\nSuggestion: where ya at\n--\n'
-                   '\nPhrase: I don\'t care.'
-                   '\nAudience: Teacher'
-                   '\nSuggestion: This isn\'t a part of my focus at the moment'
-                   '\nPhrase: ' + self.inputted_phrase +
-                   '\nAudience: ' + self.context +
-                   '\nSuggestion: ',
+                   'Phrase: Where are you?\nTone: Informal\n'
+                   'Suggestion: where you at\n--\n\nPhrase: I don\'t care.'
+                   '\nTone: Formal'
+                   '\nSuggestion: This isn\'t a scope of my focus right now'
+                   '\n--\n\nPhrase: Oh my god\nTone: Informal\n'
+                   'Suggestion: omg\n--\n\nPhrase: omg'
+                   '\nTone: Formal\nSuggestion: Oh my goodness.'
+                   '\n--\n\nPhrase: ' + self.inputted_phrase +
+                   '\nTone: ' + tone + '\nSuggestion:',
             max_tokens=100,
             temperature=0.8,
             k=0,
@@ -72,4 +55,12 @@ class SpeakEasyTranslator:
 
 
 if __name__ == "__main__":
-    pass
+    # Example usage:
+    user_input = input("Enter your desired phrase to translate: ")
+    transl = SpeakEasyTranslator(user_input)
+    formality = input("Is this in a formal or informal context? Type 'informal'"
+                      " or 'formal': ")
+    return_statement = "Another way you can say this is: " + \
+                       transl.suggest(formality).replace('-', '')
+    print(return_statement)
+
